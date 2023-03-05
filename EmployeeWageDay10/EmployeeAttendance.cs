5b908e1b9a2268835d737756395594ccae254e90
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-05 23:58:08 +0530

    Add:UC8-WagesForMultipleCompany

--- EmployeeWageDay10/EmployeeAttendance.cs
+++ EmployeeWageDay10/EmployeeAttendance.cs
@@ -6,51 +6,52 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageDay10
 {
-    public class ComputeEmpWage
+    internal class EmpWageForMultipleCompanies
     {
-        const int FULL_TIME = 1;
-        const int PART_TIME = 2;
-        const int EMP_RATE_PER_HR = 20;
-        const int MAX_WORKING_DAYS = 20;
-        const int MAX_WORKING_HRS = 100;
 
+        const int isFullTime = 1;
+        const int isPartTime = 2;
 
-        public static void ToCheckComputeEmpWage()
+        //UC8-Multiple companies
+        public static void EmpWage(string company, int empWagePerHr, int empWorkinDaysPerMonth, int empTotalWorkingHrs)
         {
-            int empHrs = 0, empWage = 0, day = 1, totalWage = 0, totalHrs = 0;
-            //UC5- WageForMonth
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int empMontlyWage = 0;
+            int totalWorkingDays = 0;
+
+            //UC1- Employee Attendance
             Random random = new Random();
-            while (day < MAX_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
+            //UC6-MaxHrs
+            while (totalWorkingDays < empWorkinDaysPerMonth && totalEmpHrs < empTotalWorkingHrs)
+
             {
-                int empAttendence = random.Next(0, 3); //0 or 1 or 2 it will generate
-                switch (empAttendence)
+                int empAttendance = random.Next(0, 3);
+                //UC3- Add Part Time Employee
+                //UC4- Switch Case
+                switch (empAttendance)
                 {
-                    case FULL_TIME:
-                        Console.WriteLine("\nFull time employee is present");
+                    case isFullTime:
+                        Console.WriteLine("Employee is FullTime");
                         empHrs = 8;
                         break;
-
-                    case PART_TIME:
-                        Console.WriteLine("\nPart time employee is present");
+                    case isPartTime:
+                        Console.WriteLine("Employee is PartTime");
                         empHrs = 4;
                         break;
-
                     default:
-                        Console.WriteLine("\nEmployee is absent");
+                        Console.WriteLine("Employee is Absent");
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HR;
-                //Console.WriteLine("Employee wage:"+empWage);  old way
-                Console.WriteLine("Day{0} Employee Wage:{1}", day, empWage);  //new way
-                                                                              //totalWage = totalWage + empWage;
-                totalWage += empWage;
-                day++;
-                totalHrs += empHrs;
+                totalWorkingDays++;
+                totalEmpHrs += empHrs;
             }
-            Console.WriteLine("Total employee wage for {0} days:{1}", day, empWage);
-
+            //UC2- Employee Daily Wage
+            //UC5- Employee Monthly Wage
+            empMontlyWage = empWagePerHr * totalEmpHrs;
+            Console.WriteLine("Company : " + company);
+            Console.WriteLine("Total Number of Hours is  " + totalEmpHrs + ", Total Number of days is " + totalWorkingDays + ", Montly Wage of Emplyoee is " + empMontlyWage);
         }
     }
-
-
 }
+   
--- EmployeeWageDay10/Program.cs
+++ EmployeeWageDay10/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            ComputeEmpWage.ToCheckComputeEmpWage();
+            Console.WriteLine("....EMP WAGE COMPUTATION....");
+
+            //UC7-Use Class Method and Variables
+
+            EmpWageForMultipleCompanies.EmpWage("HCL", 30, 16, 60);
+            EmpWageForMultipleCompanies.EmpWage("TCS", 20, 15, 80);
+            EmpWageForMultipleCompanies.EmpWage("BRIDGELAB", 15, 18, 100);
+            Console.ReadLine();
         }
     }
 
