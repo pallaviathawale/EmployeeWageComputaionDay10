ef8a6739ba8b8f3fd774c6919a0934d3fabd9d39
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-06 00:08:36 +0530

    Add: UC10-EmployeeWageForMltipleCompany

--- EmployeeWageDay10/Program.cs
+++ EmployeeWageDay10/Program.cs
@@ -7,10 +7,18 @@
             Console.WriteLine("....EMP WAGE COMPUTATION....");
 
             //UC7-Use Class Method and Variables
+            //UC8-Multiple companies
+            EmpWageBuilders Tata = new EmpWageBuilders("HCL", 30, 16, 60);
+            EmpWageBuilders Google = new EmpWageBuilders("TCS", 20, 15, 80);
+            EmpWageBuilders Apple = new EmpWageBuilders("BRIDGELAB", 15, 18, 100);
 
-            EmpWageForMultipleCompanies.EmpWage("HCL", 30, 16, 60);
-            EmpWageForMultipleCompanies.EmpWage("TCS", 20, 15, 80);
-            EmpWageForMultipleCompanies.EmpWage("BRIDGELAB", 15, 18, 100);
+            Tata.EmpWage();
+            Console.WriteLine(Tata.ToString());
+            Google.EmpWage();
+            Console.WriteLine(Apple.ToString());
+            Apple.EmpWage();
+            Console.WriteLine(Google.ToString());
+
             Console.ReadLine();
 
         }
