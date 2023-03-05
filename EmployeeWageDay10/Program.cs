213c213ba5ad532089f782eb054e72ccf3e4d5f8
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-05 23:24:21 +0530

    Add:Welcome File

---
+++ EmployeeWageDay10/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageDay10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computaion Day 10");
+        }
+    }
+}
