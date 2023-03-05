a946553ad5b28c3e7bb9c529fcf0803687611024
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-05 23:27:28 +0530

    Add:UC1-EmployeeAttendace

--- EmployeeWageDay10/Program.cs
+++ EmployeeWageDay10/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computaion Day 10");
+            EmployeeAttendance.EmployeeCheck();
         }
     }
 }
