e3b528c5d490227c8421cb0f6dbfe8862dea041f
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-05 23:51:58 +0530

    Add:UC7-ComputeEmployeeWage

--- EmployeeWageDay10/EmployeeAttendance.cs
+++ EmployeeWageDay10/EmployeeAttendance.cs
@@ -6,23 +6,21 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageDay10
 {
-
-    internal class EmpWageMaxHrs
+    public class ComputeEmpWage
     {
-        public static void CalculateMaxWorkingHrs()
+        const int FULL_TIME = 1;
+        const int PART_TIME = 2;
+        const int EMP_RATE_PER_HR = 20;
+        const int MAX_WORKING_DAYS = 20;
+        const int MAX_WORKING_HRS = 100;
+
+
+        public static void ToCheckComputeEmpWage()
         {
-            const int FULL_TIME = 1;
-            const int PART_TIME = 2;
-            const int EMP_RATE_PER_HR = 20;
-            const int MAX_WORKING_DAYS = 20;
-            const int MAX_WORKING_HRS = 100;
             int empHrs = 0, empWage = 0, day = 1, totalWage = 0, totalHrs = 0;
-
-            Console.WriteLine("Welcome to EmployeeWage Computation");
-
             //UC5- WageForMonth
             Random random = new Random();
-            while (day <= MAX_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
+            while (day < MAX_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
             {
                 int empAttendence = random.Next(0, 3); //0 or 1 or 2 it will generate
                 switch (empAttendence)
@@ -44,13 +42,15 @@ namespace EmployeeWageDay10
                 empWage = empHrs * EMP_RATE_PER_HR;
                 //Console.WriteLine("Employee wage:"+empWage);  old way
                 Console.WriteLine("Day{0} Employee Wage:{1}", day, empWage);  //new way
-                //totalWage = totalWage + empWage;
+                                                                              //totalWage = totalWage + empWage;
                 totalWage += empWage;
                 day++;
                 totalHrs += empHrs;
             }
             Console.WriteLine("Total employee wage for {0} days:{1}", day, empWage);
-            Console.ReadLine();
+
         }
     }
+
+
 }
